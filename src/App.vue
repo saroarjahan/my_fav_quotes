da5c7cf4e09f07257634eddec585061f0317d211
Saroar Jahan <saroarjahan.bd@gmail.com>
2021-05-25 22:46:26 +0300

    Quote_update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
 
-    <appHeader :quotes='quotes' :maxQuotes="maxQuotes"></appHeader>
-    <newQuote> </newQuote>
+    <div class="container">
+
+      <appHeader :quotes='quotes' :maxQuotes="maxQuotes"></appHeader>
+      <newQuote @quoteAdded="quoteAdded"> </newQuote>
+      <quoteGrid :quotes='quotes'> </quoteGrid>
+
+    </div>
       
     
   </div>
@@ -11,24 +16,33 @@
 <script>
 import header from './components/Header.vue';
 import newQuote from './components/newQuote.vue';
+import quoteGrid from './components/quoteGrid.vue';
 
 export default {
   name: 'app',
   data: function () {
             return {
                 quotes: [
-                    'Just a Quote to see something'
+                    'I wish I had done everything on earth with you. – F. Scott Fitzgerald.', 'For when the cold wind blows, I will close my eyes calmly, knowing I am anchored to you. – Tyler Knott Gregson.'
                 ],
                 maxQuotes: 100
             }
         },
         methods: {
 
+            quoteAdded(quote){
+
+              this.quotes.push(quote);
+                
+
+            },
         },
+
         components: {
 
             appHeader: header,
             newQuote: newQuote,
+            quoteGrid: quoteGrid,
         }
 }
 </script>
---
+++ src/components/quoteGrid.vue
@@ -0,0 +1,38 @@
+<template>
+
+                <div class="row">
+                    <div class='quote_box' v-for="(quote, index) in quotes" >{{quote}}</div>
+                </div>
+
+</template>
+
+<script>
+   
+    export default {
+        props: ['quotes'],
+
+        methods: {
+
+
+
+        }
+    }
+</script>
+
+<style>
+
+    .quote_box {
+        font-family: cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+
+</style>
