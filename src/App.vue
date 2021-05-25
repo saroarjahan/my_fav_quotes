cd93b68a8c30dc8166eadd6f69cac4dd85b6aa68
Saroar Jahan <saroarjahan.bd@gmail.com>
2021-05-25 21:19:42 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
 
-    <div v-for="item in quotes"> {{item}}</div>
-
-
+    <app-header :maxQuotes="maxQuotes"></app-header>
+      
     
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
   name: 'app',
-  data () {
-    return {
-      quotes: ['my first quotes']
-    }
-  }
+  data: function () {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+
+        },
+        components: {
+
+            appHeader: Header
+        }
 }
 </script>
 
---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12" >
+            {{quotes}}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['quotes', 'maxQuotes']
+    }
+</script>
